8fb8c1e206adcba411d548d3536bdf271be1c6ce
OleksiyMunenko <oleksiymun@gmail.com>
2023-09-28 20:52:29 +0300

    fix usedDataJoi (male, female)

--- models/user.js
+++ models/user.js
@@ -51,8 +51,8 @@ const userSchema = new Schema(
     },
     sex: {
       type: String,
-      enum: ['male', 'female'],
-		default: "male"
+      enum: ['Male', 'Female'],
+		default: "Male"
     },
     levelActivity: {
       type: Number,
@@ -122,7 +122,7 @@ const addUserDataSchemaJoi = Joi.object({
 	 blood: Joi.number().valid(1, 2, 3, 4).required().messages({
 		'any.required': `Missing required blood field`,
 	 }),
-	 sex: Joi.string().valid('male', 'female').messages({
+	 sex: Joi.string().lowercase().valid('male', 'female').messages({
 		'any.required': `Missing required sex field`,
 	 }),
 	 levelActivity: Joi.number().valid(1, 2, 3, 4, 5).required().messages({
