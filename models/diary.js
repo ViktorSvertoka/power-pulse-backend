3cfd38a4d94197761429853e7fbcef2c91761ab8
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-09-29 15:41:28 +0200

    Fix (MongoDB schema)

---
+++ models/diary.js
@@ -0,0 +1,116 @@
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../helpers');
+const Joi = require('joi');
+
+const diarySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    burnedCalories: {
+      type: Number,
+    },
+    consumedCalories: {
+      type: Number,
+    },
+    doneExercisesTime: {
+      type: Number,
+    },
+
+    products: [
+      {
+        weight: {
+          type: Number,
+        },
+        calories: {
+          type: Number,
+        },
+        category: {
+          type: String,
+        },
+        title: {
+          type: String,
+        },
+        amount: {
+          type: Number,
+        },
+
+        recommend: {
+          type: Boolean,
+        },
+      },
+    ],
+
+    exercises: [
+      {
+        bodyPart: {
+          type: String,
+        },
+        equipment: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        target: {
+          type: String,
+        },
+        burnedCalories: {
+          type: Number,
+        },
+        time: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  { versionKey: false, timestamps: true }
+);
+
+diarySchema.post('save', handleMongooseError);
+
+const productSchemaJoi = Joi.object({
+  productId: Joi.string().required(),
+  date: Joi.string()
+    .regex(/^\d{2}\/\d{2}\/\d{4}$/i)
+    .required()
+    .messages({
+      'any.required': `Formate date is wrong`,
+    }),
+  calories: Joi.number().min(1).required(),
+  category: Joi.string().required(),
+  recommended: Joi.boolean().required(),
+  title: Joi.string().required(),
+  amount: Joi.number().min(1).required(),
+  weight: Joi.number().min(1).required(),
+});
+
+const exerciseSchemaJoi = Joi.object({
+  exerciseId: Joi.string().required(),
+  date: Joi.string().regex(/^\d{2}\/\d{2}\/\d{4}$/i),
+  time: Joi.number().min(1).required(),
+  burnedCalories: Joi.number().min(1),
+  bodyPart: Joi.string(),
+  equipment: Joi.string(),
+  name: Joi.string(),
+  target: Joi.string(),
+});
+
+const delProductSchemaJoi = Joi.object({
+  id: Joi.string().required(),
+  date: Joi.string().regex(/^\d{2}\/\d{2}\/\d{4}$/i),
+});
+const schemas = {
+  productSchemaJoi,
+  exerciseSchemaJoi,
+  delProductSchemaJoi,
+};
+
+const Diary = model('diary', diarySchema);
+
+module.exports = { Diary, schemas };
